c7c25de04ef7ff4f30a8d85e253ebd719c92a09e
gabiprodan@gmail.com <gabiprodan@gmail.com>
2023-11-24 10:04:20 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,25 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    cr = ['EUR','RON','USD']
+    self.ddCurrency.items = cr
 
     # Any code you write here will run before the form opens.
+
+  def ddCurrency_change(self, **event_args):
+    """This method is called when an item is selected"""
+    return self.ddCurrency.selected_value
+
+  def btnCalculate_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def costGas_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    self.label_8.text = self.costGas.text;
+    return {'gas':'{}'.format(self.costGas.text)}
